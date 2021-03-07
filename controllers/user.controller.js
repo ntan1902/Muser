b29ebd5b6dceadd41d2d71b279c9c9dd590cf081
ohnguyen2404 <50840294+ohnguyen2404@users.noreply.github.com>
2021-03-07 10:54:12 +0700

    Add User from Admin's view

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,22 +6,33 @@ const Op = Sequelize.Op;
 const User = require("../models/User");
 
 route.get("/", async (req, res) => {
-  User.findAll({ attributes: ["id", "name", "email"] })
+  await User.findAll({ attributes: ["id", "name", "email"] })
     .then((users) =>
       res.render("vwUser/index", {
         layout: "admin.hbs",
         manageUsers: true,
-        users,
+        users
       })
     )
-    .catch((err) => res.send(err));
+    // .catch((err) => res.send(err));
 });
 
 route.get("/add", async (req, res) => {
-  res.render("vwUser/add.hbs");
+  res.render("vwUser/add.hbs", {
+    layout: "admin.hbs",
+    manageUsers: true
+  });
 });
 
 route.post("/add", async (req, res) => {
+const new_user = await User.create({
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    role:req.body.role,
+    is_admin: false
+  })
+  // console.log(new_user);
   res.render("vwUser/add.hbs");
 });
 
