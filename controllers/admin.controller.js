b20564797907efe33f09271a1fb9c8375a3aadd7
ohnguyen2404 <50840294+ohnguyen2404@users.noreply.github.com>
2021-04-29 07:50:59 +0700

    fix layout admin

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,35 +1,38 @@
 const express = require("express");
 const route = express.Router();
-const checkAuthen = require("../authentication/check")
+const checkAuthen = require("../authentication/check");
 const db = require("../database/db");
-route.get("/", checkAuthen, async ( req, res)=>{
-
-  const cates = db.database().ref("Categories/")
-  const songs = db.database().ref("Songs/")
-  const test = db.database().ref("Songs/").orderByChild("createdAt").limitToLast(10);
-  const users = db.database().ref("Users/")
+route.get("/", checkAuthen, async (req, res) => {
+  const cates = db.database().ref("Categories/");
+  const songs = db.database().ref("Songs/");
+  const test = db
+    .database()
+    .ref("Songs/")
+    .orderByChild("createdAt")
+    .limitToLast(10);
+  const users = db.database().ref("Users/");
   var NumUser = 0;
-  var activeUsers =0;
+  var activeUsers = 0;
   var NumCate = 0;
   var NumSongs = 0;
   var songsArr = [];
-  cates.on("value",async (snapshot)=>{
+  cates.on("value", async (snapshot) => {
     const data = Object.values(snapshot.val());
-    NumCate = data.length
-  })
-  songs.on("value",async (snapshot)=>{
+    NumCate = data.length;
+  });
+  songs.on("value", async (snapshot) => {
     const data = Object.values(snapshot.val());
-    NumSongs = data.length
-  })
-  users.on("value",async (snapshot)=>{
+    NumSongs = data.length;
+  });
+  users.on("value", async (snapshot) => {
     const data = Object.values(snapshot.val());
-    NumUser = data.length
-    activeUsers = data.filter(user=> user.status ==="online").length
-  })
-  await test.on("value",async (snapshot)=>{
+    NumUser = data.length;
+    activeUsers = data.filter((user) => user.status === "online").length;
+  });
+  await test.on("value", async (snapshot) => {
     // songsArr.map((song,index) => ({...song, order: index+1}))
     let i = 1;
-    for(let song of Object.values(snapshot.val())){
+    for (let song of Object.values(snapshot.val())) {
       let categoryRef = db.database().ref("/Categories/" + song.categoryId);
       let artistRef = db.database().ref("/Artists/" + song.artistId);
 
@@ -41,10 +44,10 @@ route.get("/", checkAuthen, async ( req, res)=>{
         let artist = snapshot.val();
         song.artist = artist.name;
       });
-      song["order"] = i
-      songsArr.push(song)
-      i++
-     }
+      song["order"] = i;
+      songsArr.push(song);
+      i++;
+    }
     res.render("vwAdmin/index", {
       layout: "main.hbs",
       checked: true,
@@ -52,9 +55,9 @@ route.get("/", checkAuthen, async ( req, res)=>{
       NumUser: NumUser,
       songs: NumSongs,
       activeUsers,
-      songsArr
+      songsArr,
     });
-  })
+  });
 });
 
 module.exports = route;
